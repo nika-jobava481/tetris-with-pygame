284eb907070c87473a37b732e8deec6e1389de97
nika-jobava481 <66549336+nika-jobava481@users.noreply.github.com>
2023-01-08 00:15:57 +0400

    added event-listeners functionality in game loop

--- main.py
+++ main.py
@@ -148,28 +148,27 @@ class Piece(object):
         self.rotation = 0
 
 
-def create_grid(locked_positions={}):
+def create_grid(locked_positions={}): # possible of locked_positions need to change to locked_pos
     grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
     for i in range(len(grid)):
         for j in range(len(grid(i))):
             if (j, i) in locked_positions:
-                c = locked_positions[(i, j)] # possible swap of i and j in [()]
-                grid[i][j]=c
+                # possible swap of i and j in [()]
+                c = locked_positions[(i, j)]
+                grid[i][j] = c
     return grid
 
-    
-def draw_grid(surface, grid):
-    surface.fill((0,0,0))
 
-    pygame.font.init()
-    font=pygame.font.SysFont('comicsans',64)
-    label=font.render("Tetris",1,(255,255,255))
-    surface.blit(label,(top_left_x + play_width/2 - (label.get_width()/2),30))
+def draw_grid(surface, grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            pygame.draw.rect(surface,grid[i][j],(top_left_x+j*block_size,top_left_y+i*block_size,block_size,block_size),0)
-    pygame.display.update()
+            pygame.draw.rect(surface, grid[i][j], (top_left_x+j*block_size,
+                             top_left_y+i*block_size, block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255, 0, 0), (top_left_x,
+                     top_left_y, play_width, play_height), 4)
+
 
 def convert_shape_format(shape):
     pass
@@ -184,7 +183,7 @@ def check_lost(positions):
 
 
 def get_shape():
-    return random.choice(shapes)
+    return Piece(5,0,random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
@@ -199,16 +198,57 @@ def draw_next_shape(shape, surface):
     pass
 
 
-def draw_window(surface):
-    pass
+def draw_window(surface,grid):
+    surface.fill((0, 0, 0))
+
+    pygame.font.init()
+    font = pygame.font.SysFont('comicsans', 64)
+    label = font.render("Tetris", 1, (255, 255, 255))
+    surface.blit(label, (top_left_x + play_width /
+                 2 - (label.get_width()/2), 30))
+
+    draw_grid(surface,grid)
+    pygame.display.update()
 
 
-def main():
-    pass
+def main(surface):   # possiblity: surface could be removed
+    locked_positions = {}
+    grid=create_grid(locked_positions)
 
+    change_piece=False
+    run=True
+    current_piece=get_shape()
+    next_piece = get_shape()
+    clock=pygame.time.Clock()
+    fall_time=0
 
-def main_menu():
-    pass
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not valid_space(current_piece,grid):
+                        current_piece.x += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not valid_space(current_piece,grid):
+                        current_piece.x -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1                    
+                    if not valid_space(current_piece,grid):
+                        current_piece.y -= 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation +=1
+                    if not valid_space(current_piece,grid):
+                        current_piece.rotation -= 1
 
+        draw_window(surface,grid)
 
+def main_menu(win):
+    main(win)
+
+win=pygame.display.set_mode((s_width,s_height))
+pygame.display.set_caption("Tetris | Nika Jobava")
 main_menu()
