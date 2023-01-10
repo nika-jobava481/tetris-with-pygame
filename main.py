cf6a08170f33e1f59dd003d5ded49aee66728893
nika-jobava481 <jobavanika481@gmail.com>
2023-01-11 02:45:54 +0400

    added change format of the shape, check if requested space is valid and check if player lost the game

--- main.py
+++ main.py
@@ -171,15 +171,38 @@ def draw_grid(surface, grid):
 
 
 def convert_shape_format(shape):
-    pass
+    positions=[]
+    format=shape.shape[shape.rotation % len(shape.shape)]
+
+    for i,line in enumerate(format):
+        row=list(line)
+        for j,column in enumerate(row):
+            if column=="0":
+                positions.append((shape.x+j,shape.y+i))
+
+    for i,pos in enumerate(positions):
+        positions[i]=(pos[0]-2,pos[i]-4)
+        
 
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos=[[(j,i) for j in range(10)  if grid [i][j]==(0,0,0)] for i in range(20)]
+    accepted_pos=[j for sub in accepted_pos for j in sub]
+
+    formatted = convert_shape_format(shape)
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1]>-1:
+                return False
+    return True
 
 
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x,y=pos
+        if y<1:
+            return True
+    return False
 
 
 def get_shape():
@@ -257,7 +280,6 @@ def main(surface):   # possiblity: surface could be removed
 
 def main_menu(win):
     main(win)
-
 
 win = pygame.display.set_mode((s_width, s_height))
 pygame.display.set_caption("Tetris | Nika Jobava")
